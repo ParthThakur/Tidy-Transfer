e040b56fd319ef9347f4e4fd1e2af449db7fcd8a
ParthThakur <parththakur@gmail.com>
2022-05-04 19:59:26 +0100

    Migrate user login to devise

--- config/routes.rb
+++ config/routes.rb
@@ -5,14 +5,14 @@ Rails.application.routes.draw do
 
   devise_scope :user do
     authenticated :user do
-      root 'home#index', as: :authenticated_root
+      root 'users#index', as: :authenticated_root
     end
 
     unauthenticated do
       root 'devise/sessions#new', as: :unauthenticated_root
     end
   end
-  
+
   resources :transfers
   get 'users' => 'users#index'
 
