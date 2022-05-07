42b959a74687d62ca9246d858f6a359071ab3e21
ParthThakur <parththakur@gmail.com>
2022-05-07 12:29:56 +0100

    Add tests for user controller

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -1,48 +1,53 @@
 require "test_helper"
 
 class UsersControllerTest < ActionDispatch::IntegrationTest
+  include Devise::Test::IntegrationHelpers
+
   setup do
-    @user = users(:one)
+    @user = users(:jane)
+    @john = users(:john)
+
+    sign_in @user
+    post user_session_path
+
   end
 
   test "should get index" do
+    # Getting a list of all users should be prohibited
     get users_url
-    assert_response :success
+
+    assert_redirected_to user_url(@user)
   end
 
-  test "should get new" do
-    get new_user_url
-    assert_response :success
+  test "get other user profile" do
+    # Accessing ids of other users redirects to current user.
+    get user_url(@john)
+
+    assert_redirected_to user_url(@user)
   end
 
   test "should create user" do
+    # Logout setup user first.
+    delete destroy_user_session_path
+
     assert_difference("User.count") do
-      post new_user_path, params: { user: { email: @user.email, password: "secret", password_confirmation: "secret" } }
+      post user_registration_path, params: { user: { email: 'new.user@example.org', password: "secret", password_confirmation: "secret" } }
     end
 
-    assert_redirected_to user_url(User.last)
+    assert_redirected_to authenticated_root_path
   end
 
-  test "should show user" do
-    get user_url(@user)
-    assert_response :success
-  end
+  test "should user logout" do
+    delete destroy_user_session_path
 
-  test "should get edit" do
-    get edit_user_url(@user)
-    assert_response :success
-  end
-
-  test "should update user" do
-    patch user_url(@user), params: { user: { email: @user.email, password: "secret", password_confirmation: "secret" } }
-    assert_redirected_to user_url(@user)
+    assert_redirected_to unauthenticated_root_path
   end
 
   test "should destroy user" do
     assert_difference("User.count", -1) do
-      delete user_url(@user)
+      delete user_registration_path
     end
 
-    assert_redirected_to users_url
+    assert_redirected_to unauthenticated_root_path
   end
 end
--- test/test_helper.rb
+++ test/test_helper.rb
@@ -9,5 +9,8 @@ class ActiveSupport::TestCase
   # Setup all fixtures in test/fixtures/*.yml for all tests in alphabetical order.
   fixtures :all
 
-  # Add more helper methods to be used by all tests here...
+  # Setup Devise login features.
+  include Devise::Test::IntegrationHelpers
+  include Warden::Test::Helpers
+
 end
